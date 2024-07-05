8adec3682d44cd86514b166dce9e02b04328ac31
shobhit1502 <shobhit.bansal.150202@gmail.com>
2024-07-06 02:19:16 +0530

    todo demo git

---
+++ src/main/java/com/lcwd/first/controllers/HomeController.java
@@ -0,0 +1,40 @@
+package com.lcwd.first.controllers;
+
+import com.lcwd.first.config.LCWDConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+@RestController
+public class HomeController {
+    @Value("${lcwd.profile.image.path}")
+    private String profilePath;
+
+    @Autowired
+    private LCWDConfig lcwdConfig;
+    @RequestMapping("/todos")
+     public List<String> justTest(){
+         List<String> todos = new ArrayList<>();
+         todos.add("Learn java...");
+         todos.add("Learn springBoot...");
+         todos.add("Make projects...");
+
+
+         //List<String> todos = Arrays.asList("Hello","hii","byeee")
+         return todos;
+
+     }
+
+     @RequestMapping("/profile-path")
+     public String getProfilePath(){
+        return profilePath;
+     }
+   @RequestMapping("/lcwd-config")
+    public LCWDConfig getLcwdConfig() {
+        return this.lcwdConfig;
+    }
+}
